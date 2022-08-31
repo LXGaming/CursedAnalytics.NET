d80e5b216703222b3ffa9934f1b3bb2dc651e5a4
Alex Thomson <LXGaming@users.noreply.github.com>
2022-08-31 20:50:16 +1200

    Set default MySql connection string

--- LXGaming.CursedAnalytics/Configuration/Config.cs
+++ LXGaming.CursedAnalytics/Configuration/Config.cs
@@ -6,7 +6,9 @@ namespace LXGaming.CursedAnalytics.Configuration;
 public class Config {
 
     [JsonPropertyName("connectionStrings")]
-    public Dictionary<string, string> ConnectionStrings { get; init; } = new() { { "MySql", "" } };
+    public Dictionary<string, string> ConnectionStrings { get; init; } = new() {
+        { "MySql", "server=127.0.0.1;port=3306;database=curse_analytics;userid=curse_analytics;password=password;pooling=true;maximumpoolsize=2;minimumpoolsize=1;" }
+    };
 
     [JsonPropertyName("services")]
     public ServiceCategory ServiceCategory { get; init; } = new();
