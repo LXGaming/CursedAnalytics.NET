ddf99d2244035f9e8c2c4a8d5773f02e20a9ce76
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-05-02 01:22:08 +1200

    Use IServiceScopeFactory

--- LXGaming.CursedAnalytics/Storage/StorageService.cs
+++ LXGaming.CursedAnalytics/Storage/StorageService.cs
@@ -4,10 +4,10 @@ using Microsoft.Extensions.Logging;
 
 namespace LXGaming.CursedAnalytics.Storage;
 
-public class StorageService(ILogger<StorageService> logger, IServiceProvider serviceProvider) : IHostedService {
+public class StorageService(ILogger<StorageService> logger, IServiceScopeFactory serviceScopeFactory) : IHostedService {
 
     public async Task StartAsync(CancellationToken cancellationToken) {
-        await using var scope = serviceProvider.CreateAsyncScope();
+        await using var scope = serviceScopeFactory.CreateAsyncScope();
         await using var storageContext = scope.ServiceProvider.GetRequiredService<StorageContext>();
         if (await storageContext.Database.EnsureCreatedAsync(cancellationToken)) {
             logger.LogInformation("Database Created");
