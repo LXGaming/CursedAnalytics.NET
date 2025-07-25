9d296bcee0638fc6c0e5df1b9fdade6602d032bb
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-07-25 21:49:28 +1200

    Add migrations

---
+++ LXGaming.CursedAnalytics/Storage/Migrations/20250617073912_InitialCreate.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LXGaming.CursedAnalytics.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Slug = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProjectDownloads",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProjectId = table.Column<long>(type: "bigint", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Value = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectDownloads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectDownloads_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProjectPopularity",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProjectId = table.Column<long>(type: "bigint", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Rank = table.Column<long>(type: "bigint", nullable: false),
+                    Score = table.Column<decimal>(type: "decimal(26,16)", nullable: false),
+                    ThumbsUp = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectPopularity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectPopularity_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectDownloads_ProjectId_Timestamp",
+                table: "ProjectDownloads",
+                columns: new[] { "ProjectId", "Timestamp" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectPopularity_ProjectId_Timestamp",
+                table: "ProjectPopularity",
+                columns: new[] { "ProjectId", "Timestamp" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_Slug",
+                table: "Projects",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectDownloads");
+
+            migrationBuilder.DropTable(
+                name: "ProjectPopularity");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
